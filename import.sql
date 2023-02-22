cae81697198dd3a6b0906b88db202e11c2596dec
TorteBiPe <74989729+TorteBiPe@users.noreply.github.com>
2023-02-22 14:47:44 +0000

    Add files via upload

---
+++ import.sql
@@ -0,0 +1,15 @@
+INSERT INTO product( name, price,urlimage)VALUES ( 'Veggy Friends VEGAN COLD CUTS "Salami Art", 80g','12.99','https://www.vantastic-foods.com/media/image/68/33/6c/A007958_200x200.jpg');
+INSERT INTO product( name, price,urlimage)VALUES ( 'OATLY OAT Barista Edition','10.99','https://www.vantastic-foods.com/media/image/9d/1c/27/A005656_1_200x200.jpg');
+INSERT INTO product( name, price,urlimage)VALUES ( 'Revo SALMON FROM PLANTS, 80g','8.99','https://www.vantastic-foods.com/media/image/2a/58/9d/A007944_200x200.jpg');
+INSERT INTO product( name, price,urlimage)VALUES ( 'Hobelz VEGAN HOT DOGS Classic, 200g','22.99','https://www.vantastic-foods.com/media/image/81/0e/dd/A000004_200x200.jpg');
+INSERT INTO product( name, price,urlimage)VALUES ( 'Vantastic foods VANTASTIC GRANULES from soy, 300g','16.99','https://www.vantastic-foods.com/media/image/f0/7c/eb/A006576_1_1_200x200.jpg');
+INSERT INTO product( name, price,urlimage)VALUES ( 'Wheaty VEGANE WEISSE, organic, 175g','10.99','https://www.vantastic-foods.com/media/image/8d/36/7a/A006355_1_200x200.jpg');
+INSERT INTO product( name, price,urlimage)VALUES ( 'Gü FREE FROM Chocolate Vanilla Cheesecake, 2x82g','20','https://www.vantastic-foods.com/media/image/45/4d/7a/A007409_200x200.jpg');
+INSERT INTO product( name, price,urlimage)VALUES ( 'Vantastic foods VANTASTIC SCHAKALOTTA, 100g','9.99','https://www.vantastic-foods.com/media/image/f6/f8/31/A006953_2_200x200.jpg');
+INSERT INTO product( name, price,urlimage)VALUES ( 'Simply V REIBEGENUSS, 200g','12','https://www.vantastic-foods.com/media/image/64/c3/a6/A006706_1_200x200.jpg');
+INSERT INTO product( name, price,urlimage)VALUES ( 'Vantastic foods VANTASTIC SCHNETZEL from soy, 1kg','21.50','https://www.vantastic-foods.com/media/image/b3/48/75/A000767_200x200.jpg');
+INSERT INTO product( name, price,urlimage)VALUES ( 'Beyond Meat Beyond Burger, 226g','10.99','https://www.vantastic-foods.com/media/image/8d/83/df/A007919_200x200.jpg');
+INSERT INTO product( name, price,urlimage)VALUES ( 'Schlagfix CREAM LIKE MASCARPONE, 250ml','8.99','https://www.vantastic-foods.com/media/image/4a/7f/5c/A006935_1_200x200.jpg');
+
+INSERT INTO user_login(login, password)VALUES ('admin', 1234);
+INSERT INTO user_login(login, password)VALUES ('other', 1234);
